65906a391580397db57c8349713f4b1705e724a6
Justin-K-Ellis <jlke45@gmail.com>
2025-11-01 00:26:18 +0900

    feat: create navbar and add to layout

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,12 @@
+export default function Navbar() {
+  return (
+    <div className="navbar bg-base-100 shadow-sm">
+      <div className="navbar-start">
+        <p className="btn btn-ghost text-xl">Doable</p>
+      </div>
+      <div className="navbar-end">
+        <button className="btn btn-primary">Login</button>
+      </div>
+    </div>
+  );
+}
