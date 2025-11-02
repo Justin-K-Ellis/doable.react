90f5a9a028687489d4128a83e18363bea7148301
Justin-K-Ellis <jlke45@gmail.com>
2025-11-02 22:53:18 +0900

    feat: let user create tasks

--- app/page.tsx
+++ app/page.tsx
@@ -8,51 +8,74 @@ export default function Home() {
     "Add test tasks",
     "Add a second task",
   ]);
+  const [inputTask, setInputTask] = useState("");
+
+  function handleSubmit(event: React.FormEvent) {
+    event.preventDefault();
+    setTasks((tasks) => tasks.concat(inputTask));
+  }
 
   return (
     <>
       <Title text="My Tasks" />
 
-      {/* Add tasks */}
-      <form>
-        <div className="">
-          <label htmlFor="add-task">Add Task</label>
-          <input type="text" id="add-task" />
-        </div>
-      </form>
+      <div id="wrapper" className="md:w-7/10 mx-auto">
+        {/* Add tasks */}
+        <section id="tasks-form">
+          <form onSubmit={handleSubmit}>
+            <div className="flex flex-col items-center justify-center border border-base-300 rounded my-4 py-4">
+              <label htmlFor="add-task" className="font-bold text-xl">
+                Add Task
+              </label>
+              <div className="flex flex-row justify-center gap-2">
+                <input
+                  type="text"
+                  id="add-task"
+                  value={inputTask}
+                  onChange={(e) => setInputTask(e.target.value)}
+                  className="input"
+                />
+                <button type="submit" className="btn btn-success">
+                  Add
+                </button>
+              </div>
+            </div>
+          </form>
+        </section>
 
-      {/* Display tasks */}
-      <section id="tasks">
-        <div className="overflow-x-auto rounded-box border border-base-content/5 md:w-7/10 mx-auto bg-base-200">
-          <table className="table">
-            {/* head */}
-            <thead>
-              <tr>
-                <th>Task</th>
-                {/* <th>Update</th> */}
-                <th>Delete</th>
-              </tr>
-            </thead>
-            <tbody>
-              {tasks.map((task) => (
-                <tr key={task}>
-                  <td>{task}</td>
-                  {/* <td>
+        {/* Display tasks */}
+        <section id="tasks">
+          <div className="overflow-x-auto rounded-box border border-base-content/5 bg-base-200">
+            <table className="table">
+              {/* head */}
+              <thead>
+                <tr>
+                  <th>Task</th>
+                  {/* <th>Update</th> */}
+                  <th>Delete</th>
+                </tr>
+              </thead>
+              <tbody>
+                {tasks.map((task) => (
+                  <tr key={task}>
+                    <td>{task}</td>
+                    {/* <td>
                     <button type="button" className="btn btn-info">
                       Update
                     </button>
                   </td> */}
-                  <td>
-                    <button type="button" className="btn btn-warning">
-                      Delete
-                    </button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      </section>
+                    <td>
+                      <button type="button" className="btn btn-warning">
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </section>
+      </div>
     </>
   );
 }
