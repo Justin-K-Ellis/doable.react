16c2c643954a33e92a5c9c9f766a8968f7ea7e35
Justin-K-Ellis <jlke45@gmail.com>
2025-11-02 22:38:43 +0900

    feat: sub out list of tasks

---
+++ app/components/Title.tsx
@@ -0,0 +1,3 @@
+export default function Title({ text }: { text: string }) {
+  return <h1 className="font-bold text-xl md:text-2xl text-center">{text}</h1>;
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,58 @@
+"use client";
+
+import { useState } from "react";
+import Title from "./components/Title";
+
 export default function Home() {
-  return <div>Home</div>;
+  const [tasks, setTasks] = useState<string[]>([
+    "Add test tasks",
+    "Add a second task",
+  ]);
+
+  return (
+    <>
+      <Title text="My Tasks" />
+
+      {/* Add tasks */}
+      <form>
+        <div className="">
+          <label htmlFor="add-task">Add Task</label>
+          <input type="text" id="add-task" />
+        </div>
+      </form>
+
+      {/* Display tasks */}
+      <section id="tasks">
+        <div className="overflow-x-auto rounded-box border border-base-content/5 md:w-7/10 mx-auto bg-base-200">
+          <table className="table">
+            {/* head */}
+            <thead>
+              <tr>
+                <th>Task</th>
+                {/* <th>Update</th> */}
+                <th>Delete</th>
+              </tr>
+            </thead>
+            <tbody>
+              {tasks.map((task) => (
+                <tr key={task}>
+                  <td>{task}</td>
+                  {/* <td>
+                    <button type="button" className="btn btn-info">
+                      Update
+                    </button>
+                  </td> */}
+                  <td>
+                    <button type="button" className="btn btn-warning">
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </section>
+    </>
+  );
 }
