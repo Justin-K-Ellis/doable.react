50faed6b71349bdb10f5904615e808fb10fa5816
Justin-K-Ellis <jlke45@gmail.com>
2025-11-04 22:23:02 +0900

    feat: create TaskList component

---
+++ app/components/TaskList.tsx
@@ -0,0 +1,57 @@
+import type { ITask } from "@/types";
+
+interface TaskListProps {
+  cssId: string;
+  title: string;
+  tasks: ITask[];
+  statusChoice: "Done" | "Todo";
+  toggleCompleteStatus: (task: ITask) => void;
+  deleteTask: (id: number) => void;
+}
+
+export default function TaskList(props: TaskListProps) {
+  return (
+    <section id={props.cssId}>
+      <div className="overflow-x-auto rounded-box border border-base-content/5 bg-base-200">
+        <h2 className="text-center font-bold text-xl">{props.title}</h2>
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>{props.statusChoice}</th>
+              <th>Task</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {props.tasks.map(
+              (task) =>
+                !task.done && (
+                  <tr key={task.id}>
+                    <td onClick={() => props.toggleCompleteStatus(task)}>
+                      <button
+                        type="button"
+                        className="btn btn-outline btn-success"
+                      >
+                        🟢
+                      </button>
+                    </td>
+                    <td>{task.name}</td>
+                    <td>
+                      <button
+                        type="button"
+                        onClick={() => props.deleteTask(task.id)}
+                        className="btn btn-warning"
+                      >
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                )
+            )}
+          </tbody>
+        </table>
+      </div>
+    </section>
+  );
+}
