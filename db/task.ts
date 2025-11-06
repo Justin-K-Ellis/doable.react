a63c88bee7d673bfbb6daa5e71c7e02c75af7aba
Justin-K-Ellis <jlke45@gmail.com>
2025-11-06 22:09:26 +0900

    feat: implement findById method

--- db/task.ts
+++ db/task.ts
@@ -4,10 +4,29 @@ import { ITask } from "@/types";
 class Tasks {
   constructor() {}
 
+  // == Create ==
+
+  // == Read All ==
   async findMany(): Promise<ITask[]> {
     const tasks: ITask[] = await pool.query(`SELECT * FROM tasks;`);
     return tasks;
   }
+
+  // == Read by Id ==
+  async findById(id: number): Promise<ITask> {
+    const task: ITask = await pool.query(
+      `
+        SELECT * FROM tasks
+        WHERE tasks.id = $1
+        `,
+      [id]
+    );
+    return task;
+  }
+
+  // == Update by Id ==
+
+  // == Delete ==
 }
 
 const tasks = new Tasks();
