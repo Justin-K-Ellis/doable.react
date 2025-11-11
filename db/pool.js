742192b6e9200d4ef60ec9f327d1c4b925e3800c
Justin-K-Ellis <jlke45@gmail.com>
2025-11-11 21:50:09 +0900

    feat: replace db fields with connection string

--- db/pool.js
+++ db/pool.js
@@ -2,11 +2,7 @@ import { Pool } from "pg";
 import "dotenv/config";
 
 const pool = new Pool({
-  host: process.env.POSTGRES_HOST,
-  user: process.env.POSTGRES_USER,
-  database: process.env.POSTGRES_DB,
-  password: process.env.POSTGRES_PW,
-  port: process.env.POSTGRES_PORT,
+  connectionString: process.env.DB_CONNECTION_STR,
 });
 
 export default pool;
